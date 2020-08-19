b4eafc343b23d9bf34c552999914b6322b7db992
42yeah <42yeah@gmx.fr>
2020-08-19 15:46:33 +0800

    Initial Commit

---
+++ algos/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  algos
+//
+//  Created by Hao Zhou on 19/08/2020.
+//  Copyright © 2020 42yeah. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
